9b63f3ad27712a7382fefafecfa39cc9a210769e
莲生 <113430875+LianshengTAT@users.noreply.github.com>
2025-05-05 16:21:27 +0800

    Create Connection.java

---
+++ src/Connection.java
@@ -0,0 +1,12 @@
+// 站点连接信息类
+class Connection {
+    String target;
+    String line;
+    double distance;
+
+    public Connection(String target, String line, double distance) {
+        this.target = target;
+        this.line = line;
+        this.distance = distance;
+    }
+}
